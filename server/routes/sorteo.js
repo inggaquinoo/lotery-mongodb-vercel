b52fbd02df9fec51c68ab33d9b6bf8dcf6a31ffd
inggaquinoo <inggaquinoo@gmail.com>
2023-09-19 16:36:44 -0500

    assTBTa

--- server/routes/sorteo.js
+++ server/routes/sorteo.js
@@ -160,7 +160,7 @@ router.get('/boletos',(req, res) => {
 //RUTA PARA CREAR TODOS LOS BOLETOS DE UN SORTEO
 // /api/sorteos/crearboletos
 router.post('/crearboletos', (req, res) => {
-        res.end('ESTAS EN RUTA PARA CREAR VARIOS BOLETOS DE Lotery')
+        res.end('ESTAS EN RUTA PARA CREAR VARIOS BOLETOS DE Lotery - GET/POST')
 
         //const insertarboletos = ModeloBoleto.initializeOrderedBulkOp();
        //var query = modelObj.collection.initializeOrderedBulkOp();

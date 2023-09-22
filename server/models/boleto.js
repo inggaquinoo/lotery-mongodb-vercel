dd483acbed236811e66103cccc6dbedf8c441135
inggaquinoo <inggaquinoo@gmail.com>
2023-09-21 19:34:57 -0500

    object ID

--- server/models/boleto.js
+++ server/models/boleto.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const EschemaBoleto = new mongoose.Schema({
     cliente_id: { type: String, require: false },
     empleado_id: { type: String, require: false },
-    sorteo_id: { type: String, require: false },
+    sorteo_id: { type: ObjectId, require: false },
     costo: { type: Number, require: true },
     terminos_condiciones: { type: String, require: true },
     fecha_compra: { type: Date, require: false },
@@ -11,3 +11,5 @@ const EschemaBoleto = new mongoose.Schema({
 })
 
 module.exports = mongoose.model('boletos',EschemaBoleto)
+
+    //sorteo_id: { type: String, require: false },

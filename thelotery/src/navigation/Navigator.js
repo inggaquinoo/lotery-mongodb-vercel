f2d98a9415c5eb4502f499ea2720694725cd3ea0
inggaquinoo <inggaquinoo@gmail.com>
2023-09-29 20:06:25 -0500

    pruebas

--- thelotery/src/navigation/Navigator.js
+++ thelotery/src/navigation/Navigator.js
@@ -17,6 +17,12 @@ const Navigator = () => {
     return (
         // headerShown: false -> oculta el encabezado de arriba del stack
         <Stack.Navigator  screenOptions={{ headerShown: true } } >
+
+            <Stack.Screen name="NuevoSorteo"  component={NuevoSorteo} />
+            <Stack.Screen name="GenerarVendedores"  component={GenerarVendedores} />
+            <Stack.Screen name="GenerarBoletos"  component={GenerarBoletos} />
+
+
             <Stack.Screen name="Login"  options={{ title: "Lotery App" }} component={Login} />
             <Stack.Screen name="ListadoSorteos"  component={ListadoSorteos} />
             <Stack.Screen name="ListadoBoletos"  component={ListadoBoletos} />
@@ -26,13 +32,7 @@ const Navigator = () => {
             <Stack.Screen name="AsignarSeries"  component={AsignarSeries} />
             <Stack.Screen name="AsignarSerieAEmpleado"  component={AsignarSerieAEmpleado} />
 
-            
-            
 
-
-            <Stack.Screen name="NuevoSorteo"  component={NuevoSorteo} />
-            <Stack.Screen name="GenerarVendedores"  component={GenerarVendedores} />
-            <Stack.Screen name="GenerarBoletos"  component={GenerarBoletos} />
 
             
            
